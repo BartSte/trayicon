ec2953752353ac670781f2d6b649963135ba1d9a
barts <b.steensma@fleetcleaner.com>
2024-06-19 16:25:57 +0200

    feature: when clicking the icon, process info is displayed

--- include/gui.hpp
+++ include/gui.hpp
@@ -2,18 +2,25 @@
 
 #include <QSystemTrayIcon>
 #include <qmenu.h>
-#include <qmessagebox.h>
 #include <qobject.h>
+#include <qprocess.h>
 
 class Gui : public QSystemTrayIcon {
   Q_OBJECT
 
-  QString command;
+  const QString command;
   QMenu menu;
+  const QProcess &process;
 
  public:
   explicit Gui(const QString command, const QString icon,
-               QObject *parent = nullptr);
-  void show_start_message(int duration_ms = 2000);
-  void on_activated(QSystemTrayIcon::ActivationReason reason);
+               const QProcess &process, QObject *parent = nullptr);
+
+  void show_start_message(int duration_ms = 2000,
+                          const Gui::MessageIcon icon = Gui::Information);
+
+  void show_process_info(int duration_ms = 10000,
+                         const Gui::MessageIcon icon = Gui::NoIcon);
+
+  void on_activated(Gui::ActivationReason reason);
 };
--- src/gui.cpp
+++ src/gui.cpp
@@ -1,10 +1,11 @@
 #include <gui.hpp>
-#include <qmessagebox.h>
 #include <qobject.h>
+#include <qsystemtrayicon.h>
 #include <spdlog/spdlog.h>
 
-Gui::Gui(const QString command, const QString icon, QObject *parent)
-    : QSystemTrayIcon(parent), command(command) {
+Gui::Gui(const QString command, const QString icon, const QProcess &process,
+         QObject *parent)
+    : QSystemTrayIcon(parent), command(command), process(process) {
   setIcon(QIcon(icon));
   setToolTip("Running command: " + command);
   setContextMenu(&menu);
@@ -15,13 +16,20 @@ Gui::Gui(const QString command, const QString icon, QObject *parent)
   spdlog::info("Tooltip set to: {}", command.toStdString());
 }
 
-void Gui::show_start_message(int duration_ms) {
-  showMessage("Start", toolTip(), QSystemTrayIcon::Information, duration_ms);
+void Gui::show_start_message(int duration_ms, Gui::MessageIcon icon) {
+  showMessage("Process started", toolTip(), icon, duration_ms);
 }
 
-void Gui::on_activated(QSystemTrayIcon::ActivationReason reason) {
-  if (reason == QSystemTrayIcon::Trigger) {
+void Gui::show_process_info(int duration_ms, Gui::MessageIcon icon) {
+  QString info = "PID: " + QString::number(process.processId()) + "\n";
+  info += "Program: " + process.program() + "\n";
+  info += "Arguments: " + process.arguments().join(" ") + "\n";
+  showMessage("Process info", info, icon, duration_ms);
+}
+
+void Gui::on_activated(Gui::ActivationReason reason) {
+  if (reason == Gui::Trigger) {
     spdlog::info("Icon clicked.");
-    show_start_message();
+    show_process_info();
   }
 }
